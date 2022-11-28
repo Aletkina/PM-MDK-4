54f428d1f0a05cd26d387371decc6cc657ff2ab6
HopeMax2004 <119292927+HopeMax2004@users.noreply.github.com>
2022-11-28 13:06:04 +0300

    Add files via upload

---
+++ ConsoleApplication19/ConsoleApplication19/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication19
+{
+   internal class Program
+    {
+        static void Main()
+        {
+            string name;
+           System.Console.WriteLine("Привет Яндекс");
+            name = System.Console.ReadLine();
+            System.Console.WriteLine("Приятно познакомиться");
+            System.Console.ReadLine();
+        }
+    }
+}
